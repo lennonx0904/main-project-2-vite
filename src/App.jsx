1e3be9f36f2d1fee4a3a0db4e7b95f6060519997
Andi <andi.lin.hy@gmail.com>
2023-12-26 16:07:24 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Button from "share-components/src/components/Button";
+
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>main-project-2-vite</h1>
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default App;
